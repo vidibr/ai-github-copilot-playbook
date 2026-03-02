3857869d88cce8a7c32479de3e5ca68f974fe508
Aaron Powell <me@aaron-powell.com>
2026-03-03 09:47:46 +1100

    forgot that place to update

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://awesome-copilot.github.com/",
+  site: "https://github.github.com/",
   base: "/awesome-copilot/",
   output: "static",
   integrations: [sitemap()],
